faaa41a9499a377c5cbf512de84cda748029903b
yarec <yarec@163.com>
2015-05-14 08:32:16 +0800

    v0.1

---
+++ framework/AFNetworking/AFNetworking/AFNetworking.h
@@ -0,0 +1,19 @@
+//
+//  AFNetworking.h
+//  AFNetworking
+//
+//  Created by r.t on 15/5/14.
+//  Copyright (c) 2015年 r.t. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFNetworking.
+FOUNDATION_EXPORT double AFNetworkingVersionNumber;
+
+//! Project version string for AFNetworking.
+FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNetworking/PublicHeader.h>
+
+
