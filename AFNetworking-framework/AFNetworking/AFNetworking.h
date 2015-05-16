25a8368298b38ba9d5ca27eff863a44926c33b4a
yarec <yarec@163.com>
2015-05-16 09:38:15 +0800

    u

--- AFNetworking-framework/AFNetworking/AFNetworking.h
+++ AFNetworking-framework/AFNetworking/AFNetworking.h
@@ -17,3 +17,22 @@ FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <AFNetworking/PublicHeader.h>
 
 
+#import "AFHTTPRequestOperation.h"
+#import "AFHTTPRequestOperationManager.h"
+#import "AFHTTPSessionManager.h"
+#import "AFNetworkActivityIndicatorManager.h"
+#import "AFNetworking.h"
+#import "AFNetworkReachabilityManager.h"
+#import "AFSecurityPolicy.h"
+#import "AFURLConnectionOperation.h"
+#import "AFURLRequestSerialization.h"
+#import "AFURLResponseSerialization.h"
+#import "AFURLSessionManager.h"
+#import "UIActivityIndicatorView+AFNetworking.h"
+#import "UIAlertView+AFNetworking.h"
+#import "UIButton+AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
+#import "UIKit+AFNetworking.h"
+#import "UIProgressView+AFNetworking.h"
+#import "UIRefreshControl+AFNetworking.h"
+#import "UIWebView+AFNetworking.h"
